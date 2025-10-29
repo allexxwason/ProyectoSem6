27794af6af934cc1bea88779677c7a6e72521679
allexxwason <alexvb1419@gmaill.com>
2025-10-29 18:40:19 -0500

    HU1: DTOs y controladores base creados

---
+++ src/main/java/com/proyectosem6/dto/VenueDTO.java
@@ -0,0 +1,25 @@
+package com.proyectosem6.dto;
+
+public class VenueDTO {
+    private Long id;
+    private String name;
+    private String location;
+
+    public VenueDTO() {}
+
+    public VenueDTO(Long id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+}
